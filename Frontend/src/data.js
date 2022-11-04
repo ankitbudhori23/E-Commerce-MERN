baac44b0c0c360315fb54cedda36422d051b8abf
Ankit Budhori <59205484+ankitbudhori23@users.noreply.github.com>
2022-11-04 18:06:01 +0530

    Add files via upload

---
+++ Frontend/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img:
+      "https://d2xamzlzrdbdbn.cloudfront.net/megaimg/d1d2140f-b19d-46de-98ac-97ca41b312f522201102.jpg"
+  },
+  {
+    id: 2,
+    img:
+      "https://cdn.shopclues.com/images/banners/2022/Oct/08/3_Shopclues_12thGenMainstream_Web_SC.jpg"
+  },
+  {
+    id: 3,
+    img:
+      "https://rukminim1.flixcart.com/fk-p-flap/1800/1800/image/424a3e459a4245ea.jpg?q=80"
+  }
+];
+
+export const categories = [
+  {
+    id: 1,
+    img:
+      "https://images-eu.ssl-images-amazon.com/images/G/31/img22/TVs/Jup22/Kamya/1_PC_CategoryCard_758X608_D._SY304_CB607449290_.jpg",
+    title: "TELEVISION",
+    cat: "tv"
+  },
+  {
+    id: 2,
+    img:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1663774825/Croma%20Assets/CMS/Bank%20banners/HP_BigTile_appleAtCroma_iPhone13_21sept2022_n4nonc.png/mxw_1440,f_auto",
+    title: "MOBILES",
+    cat: "mobiles"
+  },
+  {
+    id: 3,
+    img:
+      "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1666091065/Croma%20Assets/CMS/Homepage%20Banners/01_Homepage%20Bugs%20Daily/HP_2Split_FOD_NewatCroma_18oct2022_t7hbyn.png/mxw_1440,f_auto",
+    title: "LAPTOPS",
+    cat: "laptops"
+  }
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://i.imgur.com/NfuJ81l.png"
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/6mB89QU.png"
+  },
+  {
+    id: 3,
+    img: "https://i.imgur.com/7Cgt4fZ.png"
+  },
+  {
+    id: 4,
+    img: "https://i.imgur.com/xXXrWDt.png"
+  },
+  {
+    id: 5,
+    img: "https://i.imgur.com/bYdtnQv.png"
+  },
+  {
+    id: 6,
+    img: "https://i.imgur.com/SOlEtUO.png"
+  },
+  {
+    id: 7,
+    img: "https://i.imgur.com/yO8RD80.jpg"
+  },
+  {
+    id: 8,
+    img: "https://i.imgur.com/5DqlJQs.jpg"
+  }
+];
